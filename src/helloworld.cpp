b3e350684a0106c919981300dc6fcdb25759d4c3
PREM SEKHAR <84108418+premsekharponnada@users.noreply.github.com>
2021-06-07 11:27:59 +0530

    Update helloworld.cpp

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,1 +1,13 @@
 //This file has the code to print hello world in our local host
+#include "crow.h"
+
+int main()
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+
+    app.port(3000).multithreaded().run();
+}
